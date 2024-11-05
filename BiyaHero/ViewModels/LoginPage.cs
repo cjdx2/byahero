3c696cbdfc48b3fdb709c78e63daa02feffed307
Ariosor <106370454+siCarltoh@users.noreply.github.com>
2024-11-05 19:20:47 +0800

    Add project files.

---
+++ BiyaHero/ViewModels/LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiyaHero.ViewModels
+{
+    public partial class LoginPage
+    {
+    }
+}
