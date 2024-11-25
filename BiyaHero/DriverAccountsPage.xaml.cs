d824af5537311065121a69b16e954f5168f005e6
kianndono <kiannredondo@gmail.com>
2024-11-25 23:34:42 +0800

    driver photo license view in driver accounts page

--- BiyaHero/DriverAccountsPage.xaml.cs
+++ BiyaHero/DriverAccountsPage.xaml.cs
@@ -3,6 +3,40 @@ using BiyaHero.Models;
 
 namespace BiyaHero
 {
+    public class FullScreenImagePage : ContentPage
+    {
+        private double _scale = 1;
+
+        public FullScreenImagePage(byte[] imageData)
+        {
+            var image = new Image
+            {
+                Source = ImageSource.FromStream(() => new MemoryStream(imageData)),
+                Aspect = Aspect.AspectFit, // Or AspectFill if you want the image to fill the screen
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += (s, e) =>
+            {
+                if (e.Status == GestureStatus.Running)
+                {
+                    _scale = Math.Max(1, _scale * e.Scale);
+                    image.Scale = _scale;
+                }
+            };
+
+            image.GestureRecognizers.Add(pinchGesture);
+
+            Content = new StackLayout
+            {
+                Children = { image },
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+        }
+    }
     public partial class DriverAccountsPage : ContentPage
     {
         private readonly DatabaseService _databaseService;
@@ -85,10 +119,23 @@ namespace BiyaHero
         {
             await Navigation.PushAsync(new ReportsPage());
         }
-        
+
         private async void OnAdminLogoutClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new LoginPage());
+        }
+        private async void OnPhotoClicked(object sender, EventArgs e)
+        {
+            // Get the image that was clicked (from the binding context of the ImageButton)
+            var imageButton = sender as ImageButton;
+            var selectedDriver = imageButton?.BindingContext as Driver;
+
+            if (selectedDriver != null && selectedDriver.Photo != null)
+            {
+                // Create a new page to show the full-size image (assuming Photo is byte array or URI)
+                var fullScreenImagePage = new FullScreenImagePage(selectedDriver.Photo);
+                await Navigation.PushAsync(fullScreenImagePage);
+            }
         }
     }
 }
