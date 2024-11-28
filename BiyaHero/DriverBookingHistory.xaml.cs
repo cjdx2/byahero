960e690f7a03d7708f83c6daf65331bc46ae90d0
stevgrcia <sg760621@gmail.com>
2024-11-28 09:25:37 +0800

    Added booking history for rider

---
+++ BiyaHero/DriverBookingHistory.xaml.cs
@@ -0,0 +1,9 @@
+namespace BiyaHero;
+
+public partial class DriverBookingHistory : ContentPage
+{
+	public DriverBookingHistory()
+	{
+		InitializeComponent();
+	}
+}
--- BiyaHero/DriverProfile.xaml.cs
+++ BiyaHero/DriverProfile.xaml.cs
@@ -6,4 +6,9 @@ public partial class DriverProfile : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnBookingHistoryClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DriverBookingHistory());
+    }
 }
